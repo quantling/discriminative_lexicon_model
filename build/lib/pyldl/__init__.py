309ca5f0a8d81d89a7af15c49790051a1a164f28
motoki <motoki@saito.sfs.uni-tuebingen.de>
2021-11-15 21:08:13 +0100

    Added documents and distributions, which were forgot in the last step.

--- build/lib/pyldl/__init__.py
+++ build/lib/pyldl/__init__.py
@@ -13,7 +13,7 @@ from pip._vendor import pkg_resources
 
 __author__ = 'Motoki Saito'
 __author_email__ = 'motoki.saito@uni-tuebingen.de'
-__version__ = '0.0.0'
+__version__ = '0.2'
 __license__ = 'MIT'
 __description__ = 'Linear Discriminative Learning in Python'
 __classifiers__ = [
