d4d7e0715377aef8c39c80c1fa8d1884ef6503e7
Motoki Saito <motokisaito.8623@gmail.com>
2024-03-22 17:08:18 +0100

    New: pyldl.ldl.LDL.produce
    It calculates conditional semantic support for each time step (for each
    triphone if forms are defined in triphones), selects the one with the
    highest semantic support, and concatenates them to make a single word
    prediction.
    
    On branch master
    Changes to be committed:
          modified:   pyldl/ldl.py

--- pyldl/ldl.py
+++ pyldl/ldl.py
@@ -1,5 +1,6 @@
 import pyldl.mapping as lm
 import pandas as pd
+import numpy as np
 
 class LDL:
     def __init__ (self, words=None, embed=None, noise=False, events=None):
@@ -59,4 +60,51 @@ class LDL:
         self.gmat = self.gen_gmat(events)
         self.vmat = self.gen_vmat(words=None)
         return None
+
+    def produce (self, gold, word=False, roundby=10, max_attempt=50, positive=False):
+        if not isinstance(gold, np.ndarray):
+            gold = np.array(gold)
+        p = -1
+        xs = []
+        vecs = []
+        c_comp = np.zeros(self.cmat.cues.values.size)
+        for i in range(max_attempt):
+            s0 = np.matmul(c_comp, self.fmat.values)
+            if positive:
+                s0[s0<0] = 0 # It may be necessary for a small lexicon with multi-hot matrices.
+            s = gold - s0
+            g = np.matmul(self.gmat.values, np.diag(self.vmat.loc[self.vmat.current.values[p],:].values))
+            c_prod = np.matmul(s, g)
+            # c_prod[c_prod<0] = 0  # It may be necessary for a small lexicon with multi-hot matrices.
+            if (c_prod<=0).all():
+                break
+            else:
+                p = np.argmax(c_prod)
+                c_comp[p] = c_comp[p] + 1
+                xs = xs + [self.cmat.cues.values[p]]
+                vecs = vecs + [c_prod.round(roundby)]
+            if i==(max_attempt-1):
+                print('The maximum number of iterations ({:d}) reached.'.format(max_attempt))
+                break
+        df = pd.DataFrame(vecs).rename(columns={ i:j for i,j in enumerate(self.cmat.cues.values) })
+        hdr = pd.Series(xs).to_frame(name='Selected')
+        df = pd.concat([hdr, df], axis=1)
+        if word:
+            df = concat_cues(df.Selected)
+        return df
+
+def concat_cues (a):
+    assert is_consecutive(a)
+    a = pd.Series(a).str.slice(start=0, stop=1).iloc[:-1].str.cat(sep='') + pd.Series(a).iloc[-1]
+    return a
+
+def is_consecutive (a):
+    a = pd.Series(a)
+    b = a.shift(-1)
+    a = a.iloc[:-1]
+    b = b.iloc[:-1]
+    a = a.str.slice(start=1)
+    b = b.str.slice(stop=-1)
+    return (a==b).all()
+
 
