00be0a8edf65d90fadcc1585002985c165c994c6
Motoki Saito <motokisaito.8623@gmail.com>
2024-08-08 18:05:50 +0200

    Updated partial_semantic_support
    The old version was restricted to the cues contained by the input word.
    This restriction was loosened. This change is not backward-compatible.
    
    On branch master
    Changes to be committed:
          modified:   measures.py

--- pyldl/measures.py
+++ pyldl/measures.py
@@ -14,41 +14,69 @@ def functional_load (cue, fmat, word, smat, method='corr'):
         raise ValueError('method must be corr or mse.')
     return fload
 
-def partial_semantic_support (form, word, cmat, smat, fmat, gmat, vmat=None, choose_pos=None):
-    cues = pmap.to_ngram(word, len(form), unique=False, keep_order=True)
-    pos  = [ i for i,j in enumerate(cues) if j==form ]
-    if len(pos)==0:
-        raise ValueError('The specified form was not found in the specified word.')
-    elif len(pos)>1:
-        if choose_pos is None:
-            raise ValueError('The specified form was found at multiple locations of the specified word.')
-        else:
-            pos = pos[choose_pos]
-    else:
-        pos = pos[0]
+def partial_semantic_support (current_cue, *, prev_cues, current_word, cmat,
+                              smat, fmat, gmat, vmat):
+    """
+    Calculates the partial semantic support value for the cue provided by
+    'current_cue', given a certain context, namely a set of cues provided by
+    'prev_cues'.
 
-    gold = smat.loc[word,:]
+    Parameters
+    ----------
+    current_cue : str
+        The cue (e.g., triphone) you want to calculate a partial semantic
+        support value for (e.g., 'lk#' for 'walk').
+    prev_cues : list
+        A set of cues (e.g., triphones) that serve as a previous context. A
+        partial semantic support value will be calculated for a certain cue
+        (i.e., current_cue) given the context specified in this argument.
+    current_word : str
+        A string that represents the word you focus on. The cues specified by
+        current_cue and prev_cues should be a part of this word usually,
+        although any cue can be specified with any word technically.
+    cmat : xarray.core.dataarray.DataArray
+        A form matrix "C". It is assumed to have "word" and "cues" as its
+        dimenstions.
+    smat : xarray.core.dataarray.DataArray
+        A semantic matrix "S". It is assumed to have "word" and "semantics" as
+        its dimenstions.
+    fmat : xarray.core.dataarray.DataArray
+        A cue-semantic weight matrix "F". It is assumed to have "cues" and
+        "semantics" as its dimenstions.
+    gmat : xarray.core.dataarray.DataArray
+        A semantic-cue weight matrix "G". It is assumed to have "semantics" and
+        "cues" as its dimenstions.
+    vmat : xarray.core.dataarray.DataArray
+        A weight matrix between the current and next cues (i.e., "V"). It is
+        assumed to have "current" and "next" as its dimenstions.
 
-    prev_cues = cues[:pos]
-    cvec_t = np.zeros((1,cmat.shape[1]))
-    cvec_t = xr.DataArray(cvec_t, dims=('word','cues'), coords={'word':[word], 'cues':cmat.cues.values})
-    cvec_t.loc[word, prev_cues] = 1
-    shat_t_minus1 = np.matmul(np.array(cvec_t), np.array(fmat))
-    svec_t = np.array(gold) - shat_t_minus1
+    Returns
+    -------
+    p_semsup : float
+        A partial semantic support value. It represents how much a certain cue
+        is supported from the meaning of the word the cue belongs to, given a
+        set of cues as a context, which is provided by the argument
+        "prev_cues". A context is usually all the cues of the word preceding
+        the target cue. For example, in a usual application, the partial
+        semantic support from the meaning of the word "walks" to one of its
+        cues "ks#" is the degree to which "ks#" is predictable from the meaning
+        of "walks", discounted by how obvious the cue is coming after "#wa",
+        "wal", "alk", and "lks".
+    """
+    prev_cue = prev_cues[-1]
 
-    if vmat is None:
-        print('No V-matrix is provided. A V-matrix will be estimated. This process may take a while.')
-        vmat = pmap.gen_vmat(cmat.cues.values)
+    gmat_t = np.matmul(gmat.values, np.diag(vmat.loc[prev_cue,:].values))
+    gmat_t = xr.DataArray(gmat_t, dims=gmat.dims, coords=gmat.coords)
 
-    if len(prev_cues)==0:
-        prev_cues = ['']
-    gmat_t = np.matmul(np.array(gmat), np.array(np.diag(vmat.loc[prev_cues[-1],:])))
-
-    chat_t = np.matmul(svec_t, gmat_t)
-    chat_t = xr.DataArray(chat_t, dims=('word','cues'), coords={'word':[word], 'cues':cmat.cues.values})
-    semsup_t = chat_t.loc[word, form]
-
-    return float(semsup_t)
+    cvec_t = xr.DataArray(np.zeros((1, cmat.shape[1])), dims=cmat.dims, coords={'word':[current_word], 'cues':cmat.cues.values})
+    cvec_t.loc[current_word, prev_cues] = 1
+    svec_pred = cvec_t.dot(fmat)
+    svec_gold = smat.loc[[current_word],:]
+    svec_t = (svec_gold - svec_pred)
+    
+    chat_t = svec_t.dot(gmat_t)
+    p_semsup = float(chat_t.loc[current_word, current_cue].values)
+    return p_semsup
 
 def semantic_support (word, cue, chat):
     return float(chat.loc[word,cue].values)
